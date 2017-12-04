16f057f62132ddd05959cb98db0493e423472ea7
Rich Saunders <33992313+richrichard@users.noreply.github.com>
2017-12-04 16:45:42 -0500

    Removed page headings.

--- almanack.py
+++ almanack.py
@@ -9,7 +9,11 @@ almanack = bs(page.content, 'html.parser')
 
 newquotes = []
 for line in numbered.split(almanack.find("pre").contents[0]):
-    newquotes.append( line.replace("\n","").replace('*','').replace('- ','').strip() )
-
+    fixed = line.replace("\n","").replace('*','').replace('- ','')
+    fixed = re.sub("\d{0,3} POOR RICHAR[DP]'S ALMANACK\. ?\d{0,3}", "", fixed)
+    # Yes, there are TYPOS in the source. I will not try to fix those in the aphorisms.
+    # 'The most exquisite folly is made of wisdom spun too fine.'
+    newquotes.append( fixed.strip() )
+        
 # newquotes[0] contains all the front matter. The rest are the aphorisms.
 print([x for x in newquotes[1:] if "money" in x.lower()])
